fbd0a36cf3475cdb3d7dcf53a4668ef72fd56ab5
Emmanuel <emmanue.kimweli@student.moringaschool.com>
2022-08-19 11:34:21 +0300

    Complited assignment

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if(ride <= 400){
+    return "This one is on me!";
+  }
+  else if(ride <= 2500){
+    return "I will gladly take your thirty bucks.";
+  }
+   else if(ride > 2500){
+    return "No can do.";
+  };
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+    const place = "NYC";
+  return city === place ? 'Ok, sounds good.': 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let charm;
+    switch(tip){
+      case "generous":
+        return charm = "Thank you so much.";
+        break;
+      case "not as generous":
+        return charm = "Thank you."; 
+        break;
+      default: 
+         return charm = "Bye."; 
+         break;
+    }
 }
